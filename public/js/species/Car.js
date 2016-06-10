0a1513947661245f827f63d78df170f197fdfb61
Jeff Wu <makerjeffwu@gmail.com>
2016-06-10 14:22:23 -0700

    added Car.ts

---
+++ public/js/species/Car.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jeffersonwu on 6/9/16.
+ */
+"use strict";
+var Car = (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    return Car;
+}());
+exports.Car = Car;
+//# sourceMappingURL=Car.js.map
---
+++ public/js/species/Car.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by jeffersonwu on 6/9/16.
+ */
+
+export class Car {
+
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make:string, model:string, year:number) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
