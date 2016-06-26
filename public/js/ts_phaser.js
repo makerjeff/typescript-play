7c58477bbe96c4853f840814809999aea293ac28
Jeff Wu <makerjeffwu@gmail.com>
2016-06-26 00:06:16 -0700

    second tutorial, about to duplicate for the multi-state tutorial

--- public/js/ts_phaser.js
+++ public/js/ts_phaser.js
@@ -1,4 +1,29 @@
 /**
  * Created by jeffersonwu on 6/23/16.
  */
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+            create: this.create,
+            update: this.update,
+            render: this.render
+        });
+    }
+    SimpleGame.prototype.create = function () {
+        var text = 'Hello World!';
+        var style = { font: '65px Montserrat', fill: 'rgba(100,200,255,1.0)', align: 'center' };
+        this.textValue = this.game.add.text(0, 0, "0", style);
+        this.updateCount = 0;
+    };
+    SimpleGame.prototype.update = function () {
+        this.textValue.text = (this.updateCount++).toString();
+    };
+    SimpleGame.prototype.render = function () {
+        this.game.debug.text('This is drawn in render()', 0, 80);
+    };
+    return SimpleGame;
+}());
+window.addEventListener('load', function () {
+    var game = new SimpleGame();
+});
 //# sourceMappingURL=ts_phaser.js.map
--- public/js/ts_phaser.ts
+++ public/js/ts_phaser.ts
@@ -2,3 +2,38 @@
  * Created by jeffersonwu on 6/23/16.
  */
 
+class SimpleGame {
+
+    game: Phaser.Game;
+    textValue: Phaser.Text;
+    updateCount: number;
+
+
+    constructor(){
+        this.game = new Phaser.Game(800,600, Phaser.AUTO, 'content', {
+            create: this.create,
+            update: this.update,
+            render: this.render
+        });
+    }
+
+    create() {
+        var text = 'Hello World!';
+        var style = {font: '65px Montserrat', fill:'rgba(100,200,255,1.0)', align: 'center'};
+        this.textValue = this.game.add.text(0,0,"0", style);
+        this.updateCount = 0;
+    }
+
+    update() {
+        this.textValue.text = (this.updateCount++).toString();
+    }
+
+    render() {
+        this.game.debug.text('This is drawn in render()', 0, 80);
+    }
+
+}
+
+window.addEventListener('load', () => {
+    var game = new SimpleGame();
+});
